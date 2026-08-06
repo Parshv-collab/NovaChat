30feabfb25b1e505623ab600159cb922c11ffcd7
Parshv <chandariaparshv429@gmail.com>
2026-08-06 16:46:46 +0530

    Allowed Debug

--- app.py
+++ app.py
@@ -2943,4 +2943,4 @@ function formatLastSeen(iso) {
 # --------------------------------------------------------------------------
 if __name__ == "__main__":
     print("[NovaChat] Starting server on http://0.0.0.0:5000 ...")
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True , allow_unsafe_werkzeug=True) 
